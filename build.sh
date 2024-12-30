5f05aa2e88a76a7440bf3a4c559e54a9f9ab8f8a
jhonnyV-V <jhonny_varela_visbal@hotmail.com>
2024-12-30 19:55:34 -0400

    feature: create builds for arm64 and darwin os

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-go build -ldflags='-s' -o ./dist/phoemux ./
+GOOS=linux GOARCH=amd64 go build -ldflags='-s' -o ./dist/phoemux_amd64 ./
+GOOS=linux GOARCH=arm64 go build -ldflags='-s' -o ./dist/phoemux_arm64 ./
+
+GOOS=darwin GOARCH=amd64 go build -ldflags='-s' -o ./dist/phoemux_darwin_amd64 ./
+GOOS=darwin GOARCH=arm64 go build -ldflags='-s' -o ./dist/phoemux_darwin_arm64 ./
